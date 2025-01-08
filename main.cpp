b96ae658c8a88d72497a4de528e59ad9b230b561
Mister M <mailfortests2023@gmail.com>
2025-01-08 15:51:11 +0200

    ADD: split() in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,141 @@
 #include <iostream>
+#include <regex>
+#include <fstream>
+
+#include <algorithm> 
+#include <cctype>
+//#include <unordered_map>
+//#include <thread>
+//#include <vector>
+//#include <utility>
+//#include <shared_mutex>
+//#include <mutex>
+
+//#include <tbb/concurrent_unordered_map.h>
+
+//#include <random>
+
+
+
+void split()
+{
+	std::ifstream file("msg.txt");
+	
+	if (!file)
+	{
+		std::cout << "Error opening file\n";
+		return 1;
+	}
+	
+	std::string line;
+	std::regex word_regex("(\\w+)");//, std::regex::icase);
+	
+	while ( std::getline(file, line) )
+	{
+		auto first_word = std::sregex_iterator(line.begin(), line.end(), word_regex);
+		auto last_word = std::sregex_iterator();
+		
+		for (std::sregex_iterator i = first_word; i != last_word; ++i)
+		{
+			std::smatch match = *i;
+			std::string match_str = match.str();
+			std::transform(match_str.begin(), match_str.end(), match_str.begin(),
+						   [](unsigned char c) { return std::tolower(c); });
+			std::cout << match_str << '\n';
+		}
+	}
+	
+	file.close();
+}
 
 int main()
 {
-	std::cout << "Hello, world!" << std::endl;
+	split();
 	
 	return 0;
 }
+
+/*
+
+std::unordered_map<std::string, std::vector<std::pair<int,int>>> my_map;
+std::vector<std::shared_mutex> shared_mutexes;
+	
+std::string terms[] = {
+	"apple", "bicycle", "candle", "dragon", "elephant", 
+	"flower", "guitar", "honey", "island", "jungle",
+	"kitten", "lemon", "mountain", "notebook", "orange", 
+	"piano", "quartz", "rainbow", "sunflower", "tulip",
+	"umbrella", "violet", "whale", "xylophone", "yogurt", 
+	"zebra", "arrow", "butterfly", "castle", "dolphin",
+	"eagle", "falcon", "grape", "hazelnut", "igloo", 
+	"jellyfish", "kite", "lantern", "marble", "nectar"
+};
+
+std::string tmp[] = {
+	"a", "b", "c", "d", "e",
+	"f", "g", "h", "j", "k"
+};
+
+void read(int index)
+{
+	std::shared_lock<std::shared_mutex> _(shared_mutexes[index]);
+	
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(0, 39);
+	
+	int random_index = dis(gen);
+	
+	std::cout << "reader: " << terms[random_index] << std::endl;
+}
+
+void write(int index)
+{
+	std::unique_lock<std::shared_mutex> _(shared_mutexes[index]);
+	
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(0, 39);
+	
+	int random_index = dis(gen);
+	
+	std::uniform_int_distribution<> dis2(0, 9);
+	int r2 = dis2(gen);
+	
+	terms[random_index] = tmp[r2];
+	
+	std::cout << "writer: " << terms[random_index] << std::endl;
+}
+
+int main()
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(0, 9);
+		
+	unsigned int table_segments = 10;
+	for (int i = 0; i < table_segments; ++i)
+	{
+		shared_mutexes.push_back(std::shared_mutex());
+	}
+	
+	unsigned int writers_amount = 4;
+	unsigned int readers_amount = 8;
+	std::vector<std::thread> writers;
+	std::vector<std::thread> readers;
+	for (int i = 0; i < writers_amount; ++i)
+	{
+		int random_index = dis(gen);
+		writers.push_back(std::thread(write, random_index));
+	}
+	for (int i = 0; i < readers_amount; ++i)
+	{
+		int random_index = dis(gen);
+		readers.push_back(std::thread(read, random_index));
+	}
+	
+	for (auto& t : writers) { t.join(); }
+	for (auto& t : readers) { t.join(); }
+	
+	return 0;
+}*/
